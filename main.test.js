e5ad5450867e4630bb0843987569a74a8ef088b4
Pham Viet <viet2972001@gmail.com>
2021-09-15 17:55:13 +0700

    'commit'

--- main.test.js
+++ main.test.js
@@ -2,5 +2,5 @@ const fs = require("fs");
 window.document.body.innerHTML = fs.readFileSync("./index.html");
 test('count H1',()=>{
     var countH1 = document.querySelectorAll('h1').length
-    expect(countH1).toBe(4);
+    expect(countH1).toBe(3);
 })
